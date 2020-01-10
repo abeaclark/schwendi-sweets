50b394faffc299dfaff4ce10eac1da650c130d41
abeaclark <abeaclark@gmail.com>
2020-01-09 21:09:23 -0800

    adjust func sig

--- src/lambda/process-payment.js
+++ src/lambda/process-payment.js
@@ -4,7 +4,7 @@ const crypto = require('crypto');
 var rp = require('request-promise');
 const sendGridMail = require('@sendgrid/mail');
 
-module.exports.handler = async function(event, context) {
+exports.handler = async (event, context) => {
   const defaultClient = SquareConnect.ApiClient.instance;
   // Configure OAuth2 access token for authorization: oauth2
   var oauth2 = defaultClient.authentications['oauth2'];
